e68fbc4c2c35d2248dad0e304f61821f38fbc1ef
Enes <enes_kln@hotmail.com>
2021-07-14 22:16:42 +0300

    fixed bug (json parse)

--- screens/textScreen.js
+++ screens/textScreen.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useRef, useState, useEffect, useCallback } from "react";
-import { StyleSheet, View, Alert, Text } from "react-native";
+import { StyleSheet, View, Alert, ScrollView } from "react-native";
 import { Button } from "react-native-elements";
 import * as ImagePicker from "expo-image-picker";
 import { withFirebaseHOC } from "../config";
@@ -26,8 +26,8 @@ function textScreen({ firebase }) {
   const RichText = useRef();
 
   const insertRichTextBox = async (response) => {
-    let paragraphs = [];
     if (typeof response.responses[0].fullTextAnnotation === "undefined") return;
+
     response.responses[0].fullTextAnnotation.pages.forEach((page) => {
       page.blocks.forEach((block) => {
         block.paragraphs.forEach((paragraph) => {
@@ -39,28 +39,37 @@ function textScreen({ firebase }) {
               if (typeof symbol.property.detectedBreak !== "undefined") {
                 if (symbol.property.detectedBreak.type == "SPACE") {
                   line += " ";
-                }
-                if (symbol.property.detectedBreak.type == "EOL_SURE_SPACE") {
-                  line += " ";
+                } else if (
+                  symbol.property.detectedBreak.type == "EOL_SURE_SPACE"
+                ) {
+                  if (line.endsWith("-")) {
+                    line = line.slice(0, -1);
+                  } else {
+                    line += " ";
+                  }
                   para += line;
                   line = "";
-                }
-                if (symbol.property.detectedBreak.type == "LINE_BREAK") {
+                } else if (symbol.property.detectedBreak.type == "LINE_BREAK") {
+                  if (line.endsWith("-")) {
+                    line = line.slice(0, -1);
+                  } else {
+                    line += " ";
+                  }
                   para += line;
                   line = "";
                 }
               }
             });
           });
-          paragraphs.push(para);
+          para += "\n\n";
+          RichText.current?.insertText(para);
         });
       });
     });
-    let text = "";
-    paragraphs.forEach((element) => {
-      text += element + "\n\n";
-    });
-    RichText.current?.insertText(text);
+  };
+  const clearTextBox = () => {
+    RichText.current?.setContentHTML("");
+    setState({ text: "" });
   };
   /*useEffect(() => {
     if (typeof state.text !== "undefined" && state.text.length > 0) {
@@ -118,7 +127,6 @@ function textScreen({ firebase }) {
   const takePhoto = async () => {
     let pickerResult = await ImagePicker.launchCameraAsync({
       allowsEditing: true,
-      aspect: [4, 3],
     });
     if (!pickerResult.cancelled) {
       handleImagePicked(pickerResult);
@@ -139,7 +147,9 @@ function textScreen({ firebase }) {
       setUploading(true);
       let uploadUrl = await firebase.uploadImageAsync(pickerResult.uri);
       setState({ image: uploadUrl });
-      let requestedFeatures = [{ type: "TEXT_DETECTION", maxResults: 5 }];
+      let requestedFeatures = [
+        { type: "DOCUMENT_TEXT_DETECTION", maxResults: 5 },
+      ];
       let response = await firebase.submitToCloudVision(
         requestedFeatures,
         uploadUrl
@@ -156,14 +166,19 @@ function textScreen({ firebase }) {
     <View style={styles.container}>
       <Button onPress={createAlert} title="pdf" color="#1985bc" />
       <Button onPress={pickImage} title="Select Image" color="#1985bc" />
-      <RichEditor
-        disabled={uploading}
-        containerStyle={styles.editor}
-        ref={RichText}
-        style={styles.rich}
-        placeholder={"Text..."}
-        onChange={(text) => setState({ text: text })}
-      />
+      <Button onPress={takePhoto} title="Take Photo" color="#1985bc" />
+      <Button onPress={clearTextBox} title="Clear" color="#1985bc" />
+      <ScrollView nestedScrollEnabled={true}>
+        <RichEditor
+          disabled={uploading}
+          scrollEnabled={true}
+          containerStyle={styles.editor}
+          ref={RichText}
+          style={styles.rich}
+          placeholder={"Text..."}
+          onChange={(text) => setState({ text: text })}
+        />
+      </ScrollView>
       <RichToolbar
         style={[styles.richBar]}
         editor={RichText}
